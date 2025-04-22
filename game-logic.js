4d0a4fc8a741b9a728a3e5bbe2974077221e41ae
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-04-22 09:30:48 -0600

    Write function to play one round

--- game-logic.js
+++ game-logic.js
@@ -17,5 +17,23 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        console.log("Rock beats scissors, you win!");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        console.log("Paper beats rock, you win!"); 
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        console.log("Scissors beats paper, you win!");
+    } else {
+        computerScore++;
+        console.log(computerChoice + " beats " + humanChoice + ", you lose!");
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
