c1bdddb1122019f508a83a78f13dbd998aa3a662
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-04-28 14:50:55 -0600

    Modify the logic so as to meet the requirements
    Modify the logic so that the user can choose an option and play the
    game all in one button press. Make it so the game ends when either
    player reaches a score of 5 points.

--- game-logic.js
+++ game-logic.js
@@ -11,52 +11,91 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Write your choice: ");
-
-    return humanChoice;
+function updateScore() {
+    humanScoreDiv.textContent = humanScore;
+    computerScoreDiv.textContent = computerScore;
 }
 
-function playRound(humanChoice = " ", computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice === computerChoice) {
-        console.log("It's a draw!");
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore++;
-        console.log("Rock beats scissors, you win!");
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
-        console.log("Paper beats rock, you win!"); 
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore++;
-        console.log("Scissors beats paper, you win!");
-    } else {
-        computerScore++;
-        console.log(computerChoice + " beats " + humanChoice + ", you lose!");
+function checkForWinner() {
+    if(humanScore === 5 && computerScore === 5) {
+        alert("The game is over! IT'S A DRAW!");
+        restartGame();
+    } else if(humanScore === 5 && computerScore < 5) {
+        alert("The game is over! YOU WIN!");
+        restartGame();
+    } else if (computerScore === 5 && humanScore < 5) {
+        alert("The game is over! YOU LOSE!");
+        restartGame();
     }
 }
 
-function playGame() {
-    for (i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore === computerScore) {
-        console.log("The game ends in a draw!");
-    } else if (humanScore > computerScore) {
-        console.log("The game is over! You win!");
-    } else {
-        console.log("The game is over! You lose!");
-    }
-    console.log("Your score: " + humanScore);
-    console.log("The computer's score: " + computerScore);
-
+function restartGame() {
     humanScore = 0;
     computerScore = 0;
+    updateScore();
+    resultsDiv.textContent = "";
+}
+
+function playRound(clickedButton, computerChoice) {
+    let humanChoice;
+
+    switch(clickedButton) {
+        case "but_one":
+            humanChoice = "rock";
+        break;
+
+        case "but_two":
+            humanChoice = "paper";
+        break;
+
+        case "but_three":
+            humanChoice = "scissors";
+        break;
+    }
+
+    if (humanChoice === computerChoice) {
+        humanScore++;
+        computerScore++;
+        updateScore();
+        resultsDiv.textContent = "It's a draw!";
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        updateScore();
+        resultsDiv.textContent = "Rock beats scissors, you win!";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        updateScore();
+        resultsDiv.textContent = "Paper beats rock, you win!";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        updateScore();
+        resultsDiv.textContent = "Scissors beats paper, you win!";
+    } else {
+        computerScore++;
+        updateScore();
+        resultsDiv.textContent = computerChoice + " beats " + humanChoice + ", you lose!";
+    }
+    if (humanScore === 5 || computerScore === 5) {
+        setTimeout(() => {
+            checkForWinner();
+        }, 10)
+    } else {
+        checkForWinner();
+    }
+        
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-playGame();
+let resultsDiv = document.querySelector(".results");
+let buttons = document.querySelectorAll("button");
+
+let humanScoreDiv = document.querySelector(".player-score .score");
+let computerScoreDiv = document.querySelector(".pc-score .score");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice());
+    })
+})
